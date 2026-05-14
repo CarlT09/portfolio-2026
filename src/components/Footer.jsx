3ab74ea25285bb364400bfec8b2ae573472e0581
CarlT09 <carlosteixeira05@ua.pt>
2026-05-14 21:53:49 +0100

    PDF download

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="flex justify-center gap-8 mb-6 uppercase tracking-widest font-bold flex-wrap">
         <a href="https://github.com/CarlT09" target="_blank" rel="noreferrer" className="hover:text-white transition-colors">GitHub</a>
         <a href="https://www.linkedin.com/in/carlos-teixeira-1b10bb345/" target="_blank" rel="noreferrer" className="hover:text-white transition-colors">LinkedIn</a>
-        <a href="/CV_CarlosTeixeira.pdf" target="_blank" className="hover:text-white transition-colors border-b border-gray-400 hover:border-white pb-1">Download CV</a>
+        <a href="/cv_carlos_teixeira.pdf" target="_blank" className="hover:text-white transition-colors border-b border-gray-400 hover:border-white pb-1">Download CV</a>
       </div>
       <p className="tracking-widest">© 2026 CRLS+OS.</p>
     </footer>
