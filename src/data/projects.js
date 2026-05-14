9fea5f14c73ef02d5d31b3d20f1d1ba54ee9fa94
CarlT09 <carlosteixeira05@ua.pt>
2026-05-14 18:07:06 +0100

    Test: Links de streaming do Drive para os vídeos

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,7 +14,7 @@ export const projectsData = [
     year: "2024",
     role: "Front-end Development & UI Design",
     team: "4 elementos",
-    coverVideo: "/video-hortaranda.mp4",
+    coverVideo: "https://drive.google.com/uc?export=download&id=1jXkqcqXS-c3bPNVEoqmimFOfnUp1IjXt",
     videoCredits: "Vídeo promocional: Edição por Carlos Teixeira.",
     coverImage: "/apresentacoes/hortaranda/hero.png",
     description: "Plataforma educativa para horticultura urbana. O projeto destaca-se pela gestão de estado entre páginas estáticas via LocalStorage e um simulador interativo em Vanilla JS.",
@@ -42,7 +42,7 @@ export const projectsData = [
       },
       {
         type: 'video-body',
-        value: "/apresentacoes/hortaranda/HORTARANTA_SITE_GRAVACAO.mov",
+        value: "https://drive.google.com/uc?export=download&id=119UYv12U-ZOL3NEkTMdKTrVhmaS-66zu",
         caption: "Demonstração funcional do simulador e persistência de dados entre módulos."
       },
       {
